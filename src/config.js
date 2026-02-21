0946e92d7daf9ebe947f54baebea187ecaad6b90
juniorwebdev83 <juniorwebdev83@gmail.com>
2026-02-21 10:46:55 -0500

    html-added

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 export const GAS_URL =
-  "https://script.google.com/macros/s/AKfycbxQHKKu5pqCqtih5pTsS8OA9NsOfJgb-uhfk8XyqAKdF-KhlvbPHKgl4wI6MxzCpTa8/exec";
+  "https://script.google.com/macros/s/AKfycbxL0RmgHpiADt-H1wk3YhEkKJEZf7wFAnkDDJTCXOgUBYFnNxhU1PpiWlaeZhdAb3YQ/exec";
 // ✅ Demo tenant + admin (matches your login screen text)
 export const DEMO_INVITE_CODE = "SANCT-001";
 export const DEMO_ADMIN_EMAIL = "admin@church.com";
